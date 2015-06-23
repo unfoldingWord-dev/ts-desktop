6370e25f00f9a308862dfe949624762611577fd1
Joel Lonbeck <joel@neutrinographics.com>
2015-06-23 10:07:19 -0700

    reorganized the testing folders

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,11 +34,11 @@ module.exports = function (grunt) {
             test: {
                 options: {
                     reporter: 'spec',
-                    captureFile: 'results.txt', // Optionally capture the reporter output to a file
+                    captureFile: 'unit_tests/report.txt', // Optionally capture the reporter output to a file
                     quiet: false, // Optionally suppress output to standard out (defaults to false)
                     clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
                 },
-                src: ['test/**/*.js']
+                src: ['unit_tests/**/*.js']
             }
         },
         watch: {

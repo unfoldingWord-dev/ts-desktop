688a88ebe0a2bc00e855eeccc24356d1244f9257
Nathan Nichols <soccerhand@gmail.com>
2015-06-26 00:53:51 -0700

    initial setting manager

--- app/js/configurator.js
+++ app/js/configurator.js
@@ -2,48 +2,93 @@
  * Created by joel on 6/23/2015.
  */
 
+const readOnlyPrefix = 'locked|';
+const defaultPrefix = 'default|';
+
+var getValue = function(key) {
+    'use strict';
+
+    // first check for a readonly setting
+    var value = window.localStorage[readOnlyPrefix + key];
+    if (value === undefined) {
+        // then a regular setting
+        value = window.localStorage[key];
+    }
+    if (value === undefined) {
+        // lastly use a default (if present)
+        value = window.localStorage[defaultPrefix + key];
+    }
+
+    return value;
+};
+
 var configurator = {
     getString: function(key) {
         'use strict';
-        //TODO: implement
-        return '';
+        var value = getValue(key);
+        if (value === undefined) {
+            return '';
+        }
+
+        return value;
     },
     getInt: function(key) {
         'use strict';
-        //TODO: implement
-        return 0;
+        var value = getValue(key);
+        if (value === undefined) {
+            return 0;
+        }
+
+        value = parseInt(value, 10);
+        if (isNaN(value)) {
+            return 0;
+        }
+
+        return value;
     },
     getBool: function(key) {
         'use strict';
-        //TODO: implement
-        return false;
+        var value = getValue(key);
+        if (value === undefined) {
+            return false;
+        }
+
+        return value === 'true' || value === '1';
     },
-    setString: function(key, string) {
+    setValue: function(key, value) {
         'use strict';
-        //TODO: implement
+        window.localStorage[key] = value;
     },
-    setInt: function(key, int) {
+    setValueReadOnly: function(key, value) {
         'use strict';
-        //TODO: implement
+        this.setValue(readOnlyPrefix + key, value);
     },
-    setBool: function(key, bool) {
+    setValueDefault: function(key, value) {
         'use strict';
-        //TODO: implement
+        this.setValue(defaultPrefix + key, value);
     },
-    loadConfig: function(json) {
-        //TODO: implement
+    loadConfig: function(config) {
+        'use strict';
+        var keys = Object.keys(config);
+        for(var i = 0; i < keys.length; i++){
+            this.setValueReadOnly(keys[i], config[keys[i]]);
+        }
     },
-    loadDefaults: function(json) {
-        //TODO: implement
+    loadDefaults: function(config) {
+        'use strict';
+        var keys = Object.keys(config);
+        for(var i = 0; i < keys.length; i++){
+            this.setValueDefault(keys[i], config[keys[i]]);
+        }
     }
-}
+};
 
 
 exports.getString = configurator.getString;
 exports.getInt = configurator.getInt;
 exports.getBool = configurator.getBool;
-exports.setString = configurator.setString;
-exports.setInt = configurator.setInt;
-exports.setBool = configurator.setBool;
+exports.setValue = configurator.setValue;
+exports.setValueReadOnly = configurator.setValueReadOnly;
+exports.setValueDefault = configurator.setValueDefault;
 exports.loadConfig = configurator.loadConfig;
 exports.loadDefaults = configurator.loadDefaults;
--- unit_tests/configurator/configurator.js
+++ unit_tests/configurator/configurator.js
@@ -3,13 +3,15 @@
  */
 var assert = require('assert');
 var configurator = require('../../app/js/configurator');
-var config = require('./data/config');
+var config = require('./data/private');
+var defaults = require('./data/defaults');
 describe('@Configurator', function() {
     describe('@GetStringValue', function() {
         it('should retreive a string value', function() {
-            var key ='testKey',
+            var key ='testkey',
                 textExpected = 'this is a test';
             configurator.loadConfig(config);
+            configurator.loadDefaults(defaults);
             assert.equal(configurator.getString(key), textExpected);
         })
     })

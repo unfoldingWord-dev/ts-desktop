f2fbcb6ece377d4d402f3aa570ebdc5332b18339
K Builder <k@kbuildsyour.com>
2015-08-04 16:39:42 -0700

    New Indexer & Downloader.

--- app/js/downloader.js
+++ app/js/downloader.js
@@ -1,267 +1,73 @@
-var mkdirp = require('mkdirp');
-var url = require('url');
-var fs = require('fs');
-var _ = require('lodash');
-var path = require('path');
 var request = require('request');
-var traverse = require('traverse');
 var moment = require('moment');
 var utils = require('./lib/utils');
 var setPath = utils.setPath;
-var translator = require('./translator');
-var indexer = require('./indexer');
+var Indexer = require('./indexer');
+var downloaderIndex = new Indexer('download');
 
-var options;
+var downloader = {
 
-var filter;
+    downloadProjectList: function () {
+        'use strict';
+        var url = 'https://api.unfoldingword.org/ts/txt/2/catalog.json';
+        //TODO: retrieve catalogJson
+        downloaderIndex.indexProjects(catalogJson);
+    },
 
-function Resources (initResUrl, dataDir) {
-    'use strict';
-    var _this = this;
+    downloadSourceLanguageList: function (projectId) {
+        'use strict';
+        var project = downloaderIndex.getProject(projectId);
+        var url = project['lang_catalog'];
+        //TODO: retrieve catalogJson
+        downloaderIndex.indexSourceLanguages(projectId, catalogJson);
+    },
 
-    _this.downloadCounter = 0;
-    _this.finishedCB = null;
+    downloadResourceList: function (projectId, sourceLanguageId) {
+        'use strict';
+        var sourceLanguage = downloaderIndex.getSourceLanguage(projectId, sourceLanguageId);
+        var url = sourceLanguage['res_catalog'];
+        //TODO: retrieve catalogJson
+        downloaderIndex.indexResources(projectId, sourceLanguageId, catalogJson);
+    },
 
-    this.index = null;
+    downloadSource: function (projectId, sourceLanguageId, resourceId) {
+        'use strict';
+        var resource = downloaderIndex.getResource(projectId, sourceLanguageId, resourceId);
+        var url = resource['source'];
+        //TODO: retrieve catalogJson
+        downloaderIndex.indexSource(projectId, sourceLanguageId, resourceId, catalogJson);
+    },
 
+    downloadTerms: function (projectId, sourceLanguageId, resourceId) {
+        'use strict';
+        var resource = downloaderIndex.getResource(projectId, sourceLanguageId, resourceId);
+        var url = resource['terms'];
+        //TODO: retrieve catalogJson
+        downloaderIndex.indexTerms(projectId, sourceLanguageId, resourceId, catalogJson);
+    },
 
-    /*jshint validthis:true */
-    _this.rootResourceUrl = initResUrl;
-    _this.rootDir = dataDir;
+    downloadNotes: function (projectId, sourceLanguageId, resourceId) {
+        'use strict';
+        var resource = downloaderIndex.getResource(projectId, sourceLanguageId, resourceId);
+        var url = resource['notes'];
+        //TODO: retrieve catalogJson
+        downloaderIndex.indexNotes(projectId, sourceLanguageId, resourceId, catalogJson);
+    },
 
-    _this.setOptions = function (newOptions) {
-        options = _.clone(newOptions);
-    };
-
-    function mkdir (pathname, successCB) {
-
-        mkdirp(setPath(pathname, _this.rootDir), function (err) {
-            if (err) {
-                // TODO: error handler
-                // jscs:disable disallowEmptyBlocks
-            } else {
-                if (successCB) {
-                    successCB();
-                }
-            }
-        });
+    downloadCheckingQuestions: function (projectId, sourceLanguageId, resourceId) {
+        'use strict';
+        var resource = downloaderIndex.getResource(projectId, sourceLanguageId, resourceId);
+        var url = resource['checking_questions'];
+        //TODO: retrieve catalogJson
+        downloaderIndex.indexCheckingQuestions(projectId, sourceLanguageId, resourceId, catalogJson);
     }
 
+};
 
-    function getTsResource (urlString, fileUpdate, topLevel) {
-        var update = fileUpdate || true;
-
-        var urlObj = url.parse(urlString);
-        var response;
-        if (urlObj.host) {
-            request.get(urlString, {json: true}, function (err, res, body) {
-                if (!err && res.statusCode === 200) {
-                    response = {body: body, urlString: urlString};
-                    getTsResources(response, update, topLevel);
-                } else if (!err) {
-
-                    //TODO: handle non statusCode equal 200
-                    // jscs:disable disallowEmptyBlocks
-
-                } else {
-                    if (err.code === 'ETIMEDOUT') {
-
-                        getTsResource(urlString, update);
-                    }
-                }
-            });
-        }
-
-    }
-
-    function canUpdateFile (urlString) {
-
-        var urlObj = url.parse(urlString);
-        var filePath = setPath(urlObj.pathname, _this.rootDir);
-        var dateString = urlObj.query.slice(-8, -4) + '-' + urlObj.query.slice(-4, -2) + '-' + urlObj.query.slice(-2);
-        if (fs.existsSync(filePath)) {
-            var fileStat = fs.statSync(filePath);
-            if (fileStat.size === 0) {
-                return true;
-            }
-            if (fileStat.isFile()) {
-                if (moment(fileStat.birthtime).isBefore(dateString)) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    function notFiltered (key, dataObject) {
-        if (filter.length === 0) {
-            return true;
-        }
-        if (key === 'lang_catalog' && filter.length > 0) {
-            if (dataObject.indexOf(path.sep + filter[0] + path.sep) >= 0) {
-                return true;
-            }
-        } else if (key === 'res_catalog' && filter.length > 1) {
-            if (dataObject.indexOf(path.sep + filter[0] + path.sep + filter[1] + path.sep) >= 0) {
-                return true;
-            }
-        } else if (key === 'source' && filter.length > 2) {
-            if (dataObject.indexOf(path.sep + filter[0] + path.sep + filter[1] + path.sep + filter[2] + path.sep) >= 0) {
-                return true;
-            }
-        }
-
-        return false;
-
-    }
-
-    function getTsResources (response, update, topLevel) {
-        var urlString = response.urlString;
-        var body = response.body;
-        var urlObj = url.parse(urlString);
-        var newUrlObj;
-        var filePath;
-        var filesTraversed = 0;
-        mkdir(path.dirname(urlObj.pathname),
-            function () {
-                if (update) {
-                    fs.writeFileSync(setPath(urlObj.pathname, _this.rootDir), JSON.stringify(body));
-                }
-
-                if (!topLevel) {
-                    traverse(body).forEach(function (dataObject) {
-
-                        if (typeof dataObject === 'string' &&
-                            dataObject.indexOf('https') >= 0 &&
-                            dataObject.indexOf('date_modified') >= 0 &&
-                            dataObject.indexOf('usfm') < 0 &&
-                            notFiltered(this.key, dataObject)) {
-                            filesTraversed += 1;
-                            _this.downloadCounter += 1;
-                            if (dataObject.indexOf('date_modified') >= 0 && canUpdateFile(dataObject)) {
-                                getTsResource(dataObject, true);
-                            } else {
-                                var newResponse;
-                                newUrlObj = url.parse(dataObject);
-                                filePath = setPath(newUrlObj.pathname, _this.rootDir);
-                                if (fs.existsSync(filePath)) {
-                                    fs.readFile(filePath, 'utf8', function (err, data) {
-                                        if (!err) {
-                                            if (data.length > 0) {
-                                                newResponse = {body: JSON.parse(data), urlString: dataObject};
-                                                getTsResources(newResponse, false);
-                                            }
-                                        }
-                                    });
-                                } else {
-                                    getTsResource(dataObject, false);
-                                }
-                            }
-                        }
-                    });
-                }
-
-                console.log('filesTraversed = ' + filesTraversed + ' ; downloadCounter =' + _this.downloadCounter);
-                if (filesTraversed <= 0 && _this.downloadCounter <= 0) {
-                    if (_this.finishedCB) {
-                        _this.index = null;
-                        _this.finishedCB(_this);
-                    }
-                }
-                _this.downloadCounter -= 1;
-            }
-        );
-    }
-
-
-    _this.getTsResourcesFromCatalog = function (search, finishedCallBack) {
-
-        _this.finishedCB = finishedCallBack;
-        var topLevel = false;
-
-        if (search === 'projects') {
-            topLevel = true;
-            search = '';
-        }
-
-        filter = search || '';
-        if (filter === '') {
-            filter = [];
-        } else {
-            filter = filter.split('.');
-        }
-
-        _this.getTsResource(_this.rootResourceUrl, true, topLevel);
-    };
-
-
-    _this.downloadProjectList = function (cb) {
-        var urlObj = url.parse(_this.rootResourceUrl);
-        var filePath = setPath(urlObj.pathname, _this.rootDir);
-        _this.downloadProjects(function () {
-            if (fs.existsSync(filePath)) {
-                var data = fs.readFileSync(filePath, 'utf8');
-
-                if (data.length > 0) {
-                    cb(JSON.parse(data));
-                } else {
-                    cb(null);
-                }
-            }
-        });
-    };
-
-    _this.downloadLanguageList = function (proj, successCB) {
-
-        var langList = [];
-        _this.getTsResourcesFromCatalog(proj, function () {
-
-            _this.index = indexer.indexFiles('ts/txt/2/catalog.json', _this.rootDir);
-
-            translator.setResources(_this.rootDir, _this.index);
-            var resource = translator.open(proj);
-            if (resource) {
-
-                traverse(resource).forEach(function (dataObject) {
-                    if (this.key === 'slug') {
-                        langList.push(dataObject);
-                    }
-                });
-                successCB(langList);
-            } else {
-                successCB(null);
-            }
-        });
-    };
-
-    _this.downloadLanguage = function (proj, lang, callback) {
-        //TODO: This function is not complete. There are some design issues
-        if (_this.index === null) {
-            _this.index = indexer.indexFiles('ts/txt/2/catalog.json', _this.rootDir);
-        }
-        translator.setResources(_this.rootDir, _this.index);
-        var resource = translator.open(proj, lang);
-        if (resource) {
-            callback(resource);
-        } else {
-            callback(null);
-        }
-
-    };
-
-    _this.downloadProject = function (proj, callback) {
-        _this.getTsResourcesFromCatalog(proj, callback);
-    };
-
-    _this.downloadProjects = function (callback) {
-        _this.getTsResourcesFromCatalog('projects', callback);
-    };
-
-
-    return _this;
-}
-
-exports.Resources = Resources;
-
+exports.downloadProjectList = downloader.downloadProjectList;
+exports.downloadSourceLanguageList = downloader.downloadSourceLanguageList;
+exports.downloadResourceList = downloader.downloadResourceList;
+exports.downloadSource = downloader.downloadSource;
+exports.downloadTerms = downloader.downloadTerms;
+exports.downloadNotes = downloader.downloadNotes;
+exports.downloadCheckingQuestions = downloader.downloadCheckingQuestions;
--- app/js/indexer.js
+++ app/js/indexer.js
@@ -1,75 +1,152 @@
-/**
- * Created by delmarhager on 7/1/15.
- */
-var traverse = require('traverse');
 var fs = require('fs');
 var url = require('url');
+var _ = require('lodash');
+var configurator = require('./configurator');
 var utils = require('./lib/utils');
 var setPath = utils.setPath;
-var _ = require('lodash');
 
-var indexer = {
+var apiUrl = 'https://api.unfoldingword.org/ts/txt/2/';
+var dataDirPath = 'data';
+var linksJsonPath = setPath('links.json', dataDirPath);
+var sourceDirPath = 'source';
+
+var Indexer = function(indexType) {
+
+    //reassign this to indexer, set path
+    var indexer = this;
+    indexer.rootPath = setPath(indexType, configurator.getString('index_root_path')); //TODO: maybe make this not public?
 
 
-    indexFiles: function (rootNodeFile, rootDir) {
+    //internal functions
+    function openFile(filePath) {
         'use strict';
-        var index = {};
-
-        function setIndex (index, dataObject, resource) {
-
-            var newUrlObj = url.parse(dataObject);
-            var pathname = newUrlObj.pathname;
-            var propPath = _.dropRight(_.drop(newUrlObj.pathname.split('/'), 1), 1);
-            if (propPath[0] === 'ts') {
-                propPath = _.drop(propPath, 3);
-            }
-            propPath.push(resource);
-            propPath = propPath.toString().replace(/,/g, '.');
-            _.set(index, propPath, pathname);
-
-        }
-
-
-        function getTsResources (response) {
-            var body = response.body;
-            var newUrlObj;
-
-            traverse(body).forEach(function (dataObject) {
-                var filepath, newResponse;
-                if (typeof dataObject === 'string' &&
-                    dataObject.indexOf('https') >= 0 &&
-                    dataObject.indexOf('date_modified') >= 0 &&
-                    dataObject.indexOf('usfm') < 0) {
-                    newUrlObj = url.parse(dataObject);
-                    filepath = setPath(newUrlObj.pathname, rootDir);
-                    if (filepath && fs.existsSync(filepath)) {
-                        setIndex(index, dataObject, this.key);
-                        var data = fs.readFileSync(filepath, 'utf8');
-                        if (data.length > 0) {
-                            newResponse = {body: JSON.parse(data)};
-                            getTsResources(newResponse);
-                        }
-                    }
-                }
-            });
-        }
-
-        var filepath = setPath(rootNodeFile, rootDir);
-
-        if (fs.existsSync(filepath)) {
-            var data = fs.readFileSync(filepath, 'utf8'),
-                newResponse;
-            if (data.length > 0) {
-                newResponse = {body: JSON.parse(data)};
-                getTsResources(newResponse);
-            }
-
-            return index;
-        }
-
-
+        var fullPath = setPath(filePath, indexer.rootPath);
+        if (fs.existsSync(fullPath)) return fs.readFileSync(fullPath, 'utf8');
+        return null;
     }
-};
 
-exports.indexFiles = indexer.indexFiles;
-exports.index = indexer.index;
+    function openJson(filePath) {
+        'use strict';
+        var fileContents = openFile(filePath);
+        return JSON.parse(fileContents);
+    }
+
+    function saveFile(filePath, fileContents) {
+        'use strict';
+        var fullPath = setPath(filePath, indexer.rootPath);
+        fs.writeFileSync(fullPath, fileContents);
+        return true;
+    }
+
+    function saveJson(filePath, fileContents) {
+        'use strict';
+        saveFile(filePath, JSON.stringify(fileContents));
+        return true;
+    }
+
+    function incrementLink(md5Hash) {
+        'use strict';
+        var links = openJson(linksJsonPath);
+        if (!(md5Hash in links)) links[md5Hash] = 0;
+        links[md5Hash]++;
+        saveJson(linksJsonPath, links);
+    }
+
+    function decrementLink(md5Hash) {
+        'use strict';
+        var links = openJson(linksJsonPath);
+        if (md5Hash in links) links[md5Hash]--;
+        if (links[md5Hash]<1) // TODO: delete linked folder
+        saveFile(linksJsonPath, JSON.stringify(links));
+    }
+
+    function indexItems(md5Hash, catalogLinkFile, catalogJson) {
+        'use strict';
+        var items = JSON.parse(catalogJson);
+        var md5Path = setPath(md5Hash, dataDirPath);
+        saveFile(setPath(catalogLinkFile, sourceDirPath), md5Hash);
+        incrementLink(md5Hash);
+        for (var x in items) {
+            var item = items[x];
+            var fileName = item.slug || null;
+            if (fileName!=null) {
+                var filePath = md5Path + fileName + '.json';
+                var fileContent = JSON.stringify(item);
+                saveFile(filePath, fileContent);
+            }
+        });
+    }
+
+    //public functions
+    indexer.indexProjects = function (catalogJson) {
+        'use strict';
+        var md5Hash = md5(apiUrl + 'catalog.json');
+        var catalogLinkFile = setPath('projects_catalog.link', sourceDirPath);
+        indexItems(md5Hash, catalogLinkFile, catalogJson);
+    };
+
+    indexer.indexSourceLanguages = function (projectId, catalogJson) {
+        'use strict';
+        var project = indexer.getProject(projectId);
+        var catalogApiUrl = project['lang_catalog'];
+        var md5Hash = md5(catalogApiUrl);
+        var catalogLinkFile = setPath('languages_catalog.link', setPath(projectId, sourceDirPath));
+        indexItems(md5Hash, catalogLinkFile, catalogJson);
+    };
+
+    indexer.indexResources = function (projectId, sourceLanguageId, catalogJson) {
+        'use strict';
+        var sourceLanguage = indexer.getProject(projectId);
+        var catalogApiUrl = project['res_catalog'];
+        var md5Hash = md5(catalogApiUrl);
+        var catalogLinkFile = setPath('resources_catalog.link', setPath(sourceLanguageId, setPath(projectId, sourceDirPath)));
+        indexItems(md5Hash, catalogLinkFile, catalogJson);
+    };
+
+    indexer.indexSource = function (projectId, sourceLanguageId, resourceId, catalogJson) {
+        'use strict';
+        var md5Hash = md5(apiUrl + '/' + projectId + '/' + sourceLanguageId + '/' + resourceId + '/' + 'resources.json');
+        var catalogLinkFile = setPath('source.link', setPath(resourceId, setPath(sourceLanguageId, setPath(projectId, sourceDirPath))));
+        indexItems(md5Hash, catalogLinkFile, catalogJson);
+    }
+
+    indexer.getProjects = function () {
+        'use strict';
+        fs.readdirSync()
+        //TODO: return JSON[]
+    };
+
+    indexer.getSourceLanguages = function (projectId) {
+        'use strict';
+        //TODO: return JSON[]
+    };
+
+    indexer.getResources = function (projectId, sourceLanguageId) {
+        'use strict';
+        //TODO: return JSON[]
+    };
+
+    indexer.getProject = function (projectId) {
+        'use strict';
+        var md5Hash = openFile(setPath('projects_catalog.link', sourceDirPath));
+        var catalogJson = openJson(setPath(projectId+'.json', setPath(md5Hash, dataDirPath)));
+        return catalogJson;
+    };
+
+    indexer.getSourceLanguage = function (projectId, sourceLanguageId) {
+        'use strict';
+        var md5Hash = openFile(setPath('languages_catalog.link', setPath(projectId, sourceDirPath)));
+        var catalogJson = openJson(setPath(sourceLanguageId+'.json', setPath(md5Hash, dataDirPath)));
+        return catalogJson;
+    };
+
+    indexer.getResource = function (projectId, sourceLanguageid, resourceId) {
+        'use strict';
+        var md5Hash = openFile(setPath('resources_catalog.link', setPath(sourceLanguageid, setPath(projectId, sourceDirPath))));
+        var catalogJson = openJson(setPath(resourceId+'.json', setPath(md5Hash, dataDirPath)));
+        return catalogJson;
+    };
+
+}
+
+exports.Indexer = Indexer;

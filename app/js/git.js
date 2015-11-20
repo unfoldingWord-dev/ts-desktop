2959dc022bf7225c658b80223a95fc2b39c9482d
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-11-19 21:47:35 -0500

    Git init on new project
    - Initialize a git repo on project folders that don't have one already
    - Created git.js to wrap around the git commands
    - Put git initialization and its logi in projects.js

---
+++ app/js/git.js
@@ -0,0 +1,43 @@
+// git module
+
+'use strict';
+
+var path = require('path'),
+	exec = require('child_process').exec;
+
+function Git() {
+
+	return {
+
+		// Initialize folder as git repository
+		init: function(dir) {
+			var cmd = 'cd ' + dir + ' && git init';
+			exec(cmd, function(err, stdout, stderr) {
+				console.log("Git is initialized:", stdout);
+			});
+		},
+
+		// Add and commit all changed files
+		stage: function(message) {
+			var cmd = 'git add --all';
+			exec(cmd, function(err, stdout, stderr) {
+				console.log("Files are added:", stdout);
+			});
+
+			cmd = 'git commit -am "' + message + '"';
+			exec(cmd, function(err, stdout, stderr) {
+				console.log("Files are commited:", stdout);
+			});
+		},
+
+		// Push staged files to remote repo
+		push: function(remote, branch) {
+			var cmd = 'git push ' + remote + ' ' + branch;
+			exec(cmd, function(err, stdout, stderr) {
+				console.log("Files are being pushed:", stdout);
+			});
+		}
+	}
+}
+
+module.exports.Git = Git;

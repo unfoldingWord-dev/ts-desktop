61c42b5852fc6096e40c557ae29cef83cce749cb
Thomas Klemz <tjklemz@gmail.com>
2015-11-24 19:54:15 -0800

    more git code
    Former-commit-id: 3e4f4f069262b9c20b930160587e94801513eaf9

--- app/js/git.js
+++ app/js/git.js
@@ -8,37 +8,85 @@ var path = require('path'),
 
 function Git() {
 
+	function cmd(s) {
+		var str = s || '';
+
+		return {
+			cd: function (dir) {
+				return cmd(str + 'cd "' + dir + '"');
+			},
+
+			get and () {
+				return cmd(str + ' && ');
+			},
+
+			get then () {
+				return cmd(str + '; ');
+			},
+
+			get or () {
+				return cmd(str + ' || ');
+			},
+
+			do: function (c) {
+				return cmd(str + c);
+			},
+
+			run: function () {
+				return new Promise(function (resolve, reject) {
+					exec(str, function (err, stdout, stderr) {
+						var ret = {
+							stdout: stdout,
+							stderr: stderr,
+							error: err
+						};
+
+						(err && reject(ret)) || resolve(ret);
+					});
+				});
+			},
+
+			get _cmd () {
+				return str;
+			}
+		}
+	}
+
+	function readdir(dir) {
+		return new Promise(function (resolve, reject) {
+			fs.readdir(dir, function (err, files) {
+				(err && reject(err)) || resolve(files);
+			});
+		});
+	}
+
 	return {
 
 		// Initialize folder as git repository if it's not one already
 		init: function(dir) {
-			fs.readdir(dir, function(err, files) {
-				if (files.indexOf('.git') < 0) {
-					var cmd = 'cd ' + dir + ' && git init';
-					exec(cmd, function(err, stdout, stderr) {
-						console.log("Git is initialized:", stdout);
-					});
-				}
-			});
+			return readdir(dir).then(function (files) {
+				var init = cmd().cd(dir).and.do('git init'),
+					hasGitFolder = (files.indexOf('.git') >= 0);
+
+				return !hasGitFolder && init.run();
+			}).then(console.log.bind(console, 'Git is initialized'));
 		},
 
 		// Add and commit all changed files with the given message
 		stage: function(dir) {
-			var msg = new Date();
-			var cmd = 'cd ' + dir + ' && git add --all && git commit -am "' + msg + '"';
-			exec(cmd, function(err, stdout, stderr) {
-				console.log("Files are staged:", stdout);
-			});
+			var msg = new Date(),
+				stage = cmd().cd(dir)
+					.and.do('git add --all')
+					.and.do('git commit -am "' + msg + '"');
+
+			return stage.run().then(console.log.bind(console, 'Files are staged'));
 		},
 
 		// Push staged files to remote repo
 		push: function(dir, deviceId, repo) {
-			var cmd = 'cd ' + dir + ' && git push gitolite3@ts.door43.org:tS/' + deviceId + '/' + repo;
-			exec(cmd, function(err, stdout, stderr) {
-				console.log("error:", err);
-				console.log("stdout:", stdout);
-				console.log("stderr:", stderr);
-			});
+			var push = cmd().cd(dir).and.do('git push gitolite3@ts.door43.org:tS/' + deviceId + '/' + repo);
+
+			return push.run().then(console.log.bind(console, 'Files are pushed'));
 		}
 	}
 }

baa7fee8aa1fa119c7d235bfd325257b4e5b5aec
Jeremy Lane <jlane@veritasproductions.net>
2015-11-29 23:33:17 -0800

    misc cleanup
    Former-commit-id: 96de5f70ce18d036050a8bb11b1acfe7d7abff19

--- app/js/uploader.js
+++ app/js/uploader.js
@@ -40,9 +40,6 @@
         };
 
         var generateRegisterRequestString = function (keyPair, deviceId) {
-            console.log("generating request string");
-            console.log(deviceId);
-
             return JSON.stringify({
                 key: keyPair.public,
                 udid: deviceId
@@ -53,7 +50,6 @@
 
 
             return new Promise(function(resolve, reject){
-                console.log("starting keygen promise");
                 keygen({
                     location: paths.sshPath + "/ts",
                     comment: deviceId,
@@ -61,15 +57,8 @@
                 }, function(err, out){
                     if(err) return console.log('Something went wrong: '+err);
                     console.log('Keys created!');
-                    console.log('private key: '+out.key);
-                    console.log('public key: '+out.pubKey);
 
-                    var keys = {
-                        public: out.pubKey,
-                        private: out.key
-                    };
-
-                    resolve( mkdirp(paths.sshPath).then(function () {
+                    resolve(mkdirp(paths.sshPath).then(function () {
                         var writePublicKey = write(paths.publicKeyPath, out.pubKey),
                             writePrivateKey = write(paths.privateKeyPath, out.key).then(function () {
                                 return chmod(paths.privateKeyPath, '600');
@@ -77,7 +66,10 @@
 
                         return Promise.all[writePublicKey, writePrivateKey];
                     }).then(function() {
-                        return keys;
+                        return {
+                            public: out.pubKey,
+                            private: out.key
+                        };
                     }));
                 });
 
@@ -113,12 +105,10 @@
 
                 var client = net.createConnection({port: port, host: host}, function () {
                     var registrationString = generateRegisterRequestString(keys, deviceId);
-                    console.log("reg string: " + registrationString);
                     client.write(registrationString);
                 });
 
                 client.on('data', function (data) {
-                    console.log("response: " + data.toString());
                     var response = JSON.parse(data.toString());
 
                     if (response.error) {
@@ -145,27 +135,21 @@
 
             setSshPath: function(path){
                 paths.sshPath = path;
-                console.log("ssh path: " + path);
             },
 
             register: function (host, port) {
-                console.log("beginning registration");
                 var opts = {
                     host: host || 'test.door43.org',
                     port: port || 9095
                 };
 
                 return this.getDeviceId().then(function (deviceId) {
-                    console.log("got device id: " + deviceId);
-
                     return readKeyPair().then(function (keys) {
-                        console.log("found keys");
                         return {
                             keys: keys,
                             deviceId: deviceId
                         };
                     }).catch(function (err) {
-                        console.log("no keys: " + err);
                         return createKeyPair(deviceId).then(function(keys){
                             return sendRegistrationRequest(opts.host, opts.port, deviceId,keys);
                         });
@@ -181,7 +165,6 @@
             },
 
             getDeviceId: function() {
-                console.log("getting device id");
                 return new Promise(function(resolve, reject) {
                     getmac.getMac(function(err, mac) {
                         var m = mac.replace(/-|:/g, '');

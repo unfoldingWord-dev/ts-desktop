37adfed7862bb3bbcad1fb4202723714a1c99c9e
Thomas Klemz <tjklemz@gmail.com>
2015-12-11 10:59:49 -0800

    modified for debian
    Former-commit-id: aaadce0102a4c4a7873df4fafe7fc6fbad4b25a8

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+sudo apt-get install software-properties-common build-essential
 sudo add-apt-repository ppa:git-core/ppa -y
 wget -qO- https://deb.nodesource.com/setup_5.x | sudo -E bash -
-sudo apt-get install -y build-essential checkinstall git nodejs
+sudo apt-get install -y git nodejs
 
 mkdir -p ~/Desktop && cd ~/Desktop && git clone https://github.com/unfoldingWord-dev/ts-desktop
 cd ts-desktop && git checkout develop
@@ -12,4 +13,3 @@ sudo npm install -g bower
 npm install && bower install
 
 exit
-

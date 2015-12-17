b5a94a0861280318df0a49c7c92c647469d810c1
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-12-17 03:01:39 -0500

    Make settings persistent with file and local storage

--- app/js/configurator.js
+++ app/js/configurator.js
@@ -8,83 +8,53 @@
     'use strict';
 
     let _ = require('lodash');
+    let user_setting = require('../config/user-setting');
 
     function Configurator () {
         let storage = {};
 
         let getValue = function (key) {
-            if (key === undefined) {
-                return key;
-            }
-            key = key.toLowerCase();
+            if (!key) { return key; }
 
-            let valueObjStr = storage[key] || '{}';
-            let valueObj = JSON.parse(valueObjStr);
+            key = key.toLowerCase();
+            let valueObj = JSON.parse(storage[key] || '{}');
             let metaObj = valueObj.meta || {'default': ''};
 
-            //load value
-            let value = valueObj.value;
-
-            //otherwise use default (if present)
-            if (value === undefined && metaObj.default) {
-                value = metaObj.default;
-            }
-
-            return value;
+            return valueObj.value || metaObj.default;
         };
 
         let getMetaValue = function (key, metaKey) {
-            if (key === undefined) {
-                return key;
-            }
+            if (!key) { return key; }
+            
             key = key.toLowerCase();
+            let valueObj = JSON.parse(storage[key] || '{}');
 
-            let valueObjStr = storage[key] || '{}';
-            let valueObj = JSON.parse(valueObjStr);
-
-            return valueObj.meta ? valueObj.meta[metaKey] : '';
+            return valueObj.meta[metaKey] || '';
         };
 
         let setValue = function (key, value, meta) {
-            if (key === undefined || value === undefined) {
-                return;
-            }
+            if (!key || !value) { return; }
+            
             key = key.toLowerCase();
             value = typeof value === 'boolean' || typeof value === 'number' ? value : value.toString();
 
-            //return if read-only
-            let mutable = getMetaValue(key, 'mutable');
-            if (mutable !== undefined && mutable === false) {
-                return;
-            }
+            if (getMetaValue(key, 'mutable') === false) { return; }
 
-            //load value object or create new empty value object
             let emptyStorageObj = {'value': value, 'meta': {'mutable': true, 'type': typeof value, 'default': ''}};
             let valueObj = storage[key] !== undefined ? JSON.parse(storage[key]) : emptyStorageObj;
-
-            //update value
             valueObj.value = value;
-
-            //update meta
             valueObj.meta = _.merge(valueObj.meta, meta);
 
-            //update value in storage
             storage[key] = JSON.stringify(valueObj);
         };
 
         let unsetValue = function (key) {
-            if (key === undefined) {
-                return;
-            }
+            if (!key) { return; }
+
             key = key.toLowerCase();
 
-            //return if read-only
-            let mutable = getMetaValue(key, 'mutable');
-            if (mutable === false) {
-                return;
-            }
+            if (getMetaValue(key, 'mutable') === false) { return; }
 
-            //remove value from storage
             if (typeof storage.removeItem === 'function') {
                 storage.removeItem(key);
             } else {
@@ -97,45 +67,30 @@
         };
 
         let setDefaultValue = function (key, value) {
-            setValue(key, value, {'default': value});
+            setValue(key, value, {'default': value || ''});
         };
 
         let getKeys = function () {
             return Object.keys(storage);
         };
 
-        let getText = function (key) {
-            console.log('getting text for', key);
-            if (!key) {
-                return;
-            }
-            key = key.toLowerCase();
-
-            let valueObjStr = storage[key] || '{}';
-            console.log('valobjstr', valueObjStr);
-            let valueObj = JSON.parse(valueObjStr);
-            let text = valueObj.text;
-
-            console.log('returning', text);
-            return text;
-        };
-
-        let getType = function (key) {
-            if (!key) {
-                return;
-            }
-            key = key.toLowerCase();
-
-            let valueObjStr = storage[key] || '{}';
-            let valueObj = JSON.parse(valueObjStr);
-            let type = valueObj.meta.type;
-
-            return type;
-        };
-
+        // This is the returned object
         let configurator = {
+
+            _storage: function() {
+                return storage;
+            },
+
             setStorage: function (storeObject) {
                 storage = storeObject;
+            },
+
+            saveUserSetting: function(setting) {
+                storage['user-setting'] = JSON.stringify(setting);
+            },
+
+            getUserSetting: function() {
+                return JSON.parse(storage['user-setting']) || user_setting;
             },
 
             /**
@@ -144,12 +99,7 @@
              * @returns {object}
              */
             getValue: function (key) {
-                let value = getValue(key);
-                if (value === undefined) {
-                    return '';
-                }
-
-                return value;
+                return getValue(key) || '';
             },
 
             /**
@@ -161,11 +111,6 @@
             setValue: function (key, value, meta) {
                 setValue(key, value, meta);
             },
-
-
-            // getText: function (key) {
-            //     let
-            // },
 
             /**
              * Loads a configuration object into the configurator
@@ -203,35 +148,6 @@
                 for (let i = 0; i < keys.length; i++) {
                     unsetValue(keys[i]);
                 }
-            },
-
-            getSettings: function (config) {
-                var settings = [];
-                for (var group in config) {
-                    if (config.hasOwnProperty(group)) {
-                        var keyArrays = config[group];
-                        var list = [];
-                        for (var i = 0; i < keyArrays.length; i++) {
-                            var key = keyArrays[i];
-                            list.push({
-                                "name": key,
-                                "text": getText(key),
-                                "value": getValue(key),
-                                "type": getType(key),
-                                "handler": key + 'Tap'
-                            });
-                            console.log(keyArrays[i]);
-                        }
-                        settings.push({'group': group, 'list': list});
-                    }
-                }
-            //     var settings = [];
-            //     keys.forEach(function(key) {
-            //         settings.push({name: key, value: getValue(key), type: getMetaValue(key, type)});
-            //     });
-            //     return settings;
-                console.log(storage);
-                return [];
             }
 
         };

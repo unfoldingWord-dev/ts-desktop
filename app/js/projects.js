ef85c8c3783fbcbc07288c3cf44f19676880d56d
Chris Whitfield <cdwhitfield73@gmail.com>
2016-01-12 09:41:04 -0800

    Finished eliminating need for projects file

--- app/js/projects.js
+++ app/js/projects.js
@@ -137,6 +137,15 @@ function ProjectsManager(query, configurator) {
             return zipper(r);
         },
 
+        getProjectName: function (id) {
+            var r = query([
+                "select sl.project_name 'name' from project p",
+                "join source_language sl on sl.project_id=p.id",
+                "where sl.slug='en' and p.slug='" + id + "'",
+            ].join(' '));
+            return zipper(r);
+        },
+
         /**
          *  var sources = pm.sources,
          *      englishSources = _.filter(sources, 'lc', 'en'),
@@ -151,6 +160,17 @@ function ProjectsManager(query, configurator) {
                     "join project p on p.id=sl.project_id",
                     "order by r.name"
                 ].join(' '));
+            return zipper(r);
+        },
+
+        getSourceDetails: function (source) {
+            var id = source.split('-');
+            var r = query([
+                "select r.id, r.slug 'source', r.name, sl.name 'ln', sl.slug 'lc', p.slug 'project', r.checking_level 'level', r.version, r.modified_at 'date_modified' from resource r",
+                "join source_language sl on sl.id=r.source_language_id",
+                "join project p on p.id=sl.project_id",
+                "where p.slug='" + id[0] + "' and sl.slug='" + id[1] + "' and r.slug='" + id[2] + "'"
+            ].join(' '));
             return zipper(r);
         },
 
@@ -346,7 +366,7 @@ function ProjectsManager(query, configurator) {
                                 chapterContent += '//\n\n';
 
                                 chapterContent += '//\n';
-                                chapterContent += meta.project.name + '\n';
+                                chapterContent += meta.project_name + '\n';
                                 chapterContent += '//\n\n';
 
                                 chapterContent += '//\n';
@@ -355,7 +375,7 @@ function ProjectsManager(query, configurator) {
                             }
 
                             // add frame
-                            chapterContent += '{{https://api.unfoldingword.org/' + meta.project.slug + '/jpg/1/en/360px/' + meta.project.slug + '-' + meta.target_language.id + '-' + frame.meta.chapterid + '-' + frame.meta.frameid + '.jpg}}\n\n';
+                            chapterContent += '{{https://api.unfoldingword.org/' + meta.project_id + '/jpg/1/en/360px/' + meta.project_id + '-' + meta.target_language.id + '-' + frame.meta.chapterid + '-' + frame.meta.frameid + '.jpg}}\n\n';
                             chapterContent += frame.transcontent + '\n\n';
                         }
                         if(chapterContent !== '' && numFinishedFrames > 0) {
@@ -390,8 +410,6 @@ function ProjectsManager(query, configurator) {
 
         saveTargetTranslation: function (translation, meta) {
             var paths = this.getPaths(meta);
-
-            // save project.json and manifest.json
 
             // translation is an array
             // translation[0].meta.frameid
@@ -433,7 +451,7 @@ function ProjectsManager(query, configurator) {
                 },
                 package_version: 3,
                 target_language: meta.target_language,
-                project_id: meta.project.slug,
+                project_id: meta.project_id,
                 project_type: meta.project_type,
                 source_translations: sources,
                 translators: meta.translators,
@@ -473,7 +491,6 @@ function ProjectsManager(query, configurator) {
 
             return mkdirp(paths.projectDir)
                 .then(writeFile(paths.manifest, manifest))
-                .then(writeFile(paths.project, meta))
                 .then(makeChapterDirs(chunks))
                 .then(updateChunks(chunks))
                 .then(git.init.bind(git, paths.projectDir))
@@ -487,11 +504,9 @@ function ProjectsManager(query, configurator) {
         loadTargetTranslationsList: function () {
             var makePaths = config.makeProjectPathsForProject.bind(config);
 
-            // return the project.json, not the manifest.json
-
             return this.loadProjectsList()
                        .then(map(makePaths))
-                       .then(map('project'))
+                       .then(map('manifest'))
                        .then(map(read))
                        .then(Promise.all.bind(Promise))
                        .then(map(fromJSON));

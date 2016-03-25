1cc9e37d01aba341a830635396b58422e54b0e5b
Thomas Klemz <tjklemz@gmail.com>
2016-03-25 16:43:25 -0700

    registering keys
    Former-commit-id: 7f3f5e2d83a42404be18d8d76b44d4ce0551c5cb

--- src/js/uploader.js
+++ src/js/uploader.js
@@ -3,25 +3,13 @@
 'use strict';
 
 var net = require('net'),
-    jsonfile = require('jsonfile'),
-    getmac = require('getmac'),
     path = require('path'),
-    rimraf = require('rimraf'),
-    fs = require('fs'),
-    _ = require('lodash'),
     utils = require('../js/lib/util'),
+    _ = utils._,
     log = utils.log,
-    wrap = utils.promisify,
-    guard = utils.guard,
-    mkdirp = wrap(null, require('mkdirp')),
+    fs = utils.fs,
     keypair = require('keypair'),
-    forge = require('node-forge'),
-    getMac = wrap(getmac, 'getMac'),
-    write = wrap(fs, 'writeFile'),
-    read = wrap(fs, 'readFile'),
-    chmod = wrap(fs, 'chmod'),
-    readdir = wrap(fs, 'readdir'),
-    map = guard('map');
+    forge = require('node-forge');
 
 // TODO: this module has diverged from it's original intent and how the name is misleading.
 // All this module is doing is registering with the authentication server
@@ -43,18 +31,11 @@ function Uploader(dataPath) {
         }
     };
 
-    var generateRegisterRequestString = function (keyPair, deviceId) {
-        return JSON.stringify({
-            key: keyPair.public,
-            udid: deviceId
-        });
-    };
-
     var createKeyPair = function (deviceId) {
 
         let keyPath = path.join(paths.sshPath, paths.privateKeyName);
 
-        return mkdirp(paths.sshPath).then(function () {
+        return utils.mkdirp(paths.sshPath).then(function () {
             var pair = keypair(),
                 publicKey = forge.pki.publicKeyFromPem(pair.public),
                 publicSsh = forge.ssh.publicKeyToOpenSSH(publicKey, deviceId),
@@ -68,19 +49,17 @@ function Uploader(dataPath) {
         }).then(function (keys) {
             log('Keys created!');
 
-            var writePublicKey = write(paths.publicKeyPath, keys.public),
-                writePrivateKey = write(paths.privateKeyPath, keys.private).then(function () {
-                    return chmod(paths.privateKeyPath, '600');
+            var writePublicKey = fs.writeFile(paths.publicKeyPath, keys.public),
+                writePrivateKey = fs.writeFile(paths.privateKeyPath, keys.private).then(function () {
+                    return fs.chmod(paths.privateKeyPath, '600');
                 });
 
-            return Promise.all([writePublicKey, writePrivateKey]).then(function () {
-                return keys;
-            });
+            return Promise.all([writePublicKey, writePrivateKey]).then(utils.ret(keys));
         });
     };
 
     var readKeyPair = function () {
-        return readdir(paths.sshPath).then(function (files) {
+        return fs.readdir(paths.sshPath).then(function (files) {
             var hasPubKey = _.includes(files, paths.publicKeyName),
                 hasPrivateKey = _.includes(files, paths.privateKeyName),
                 hasBoth = hasPubKey && hasPrivateKey;
@@ -92,45 +71,13 @@ function Uploader(dataPath) {
             return hasBoth;
         })
         .then(function() {
-            var readPubKey = read(paths.publicKeyPath),
-                readSecKey = read(paths.privateKeyPath);
+            var readPubKey = fs.readFile(paths.publicKeyPath),
+                readSecKey = fs.readFile(paths.privateKeyPath);
 
             return Promise.all([readPubKey, readSecKey]);
         })
-        .then(map(String))
+        .then(utils.map(String))
         .then(_.zipObject.bind(_, ['public', 'private']));
-    };
-
-    var sendRegistrationRequest = function(host, port, deviceId, keys) {
-
-        return new Promise(function (resolve, reject) {
-
-            var client = net.createConnection({port: port, host: host}, function () {
-                var registrationString = generateRegisterRequestString(keys, deviceId);
-                client.write(registrationString);
-            });
-
-            client.on('data', function (data) {
-                var response = JSON.parse(data.toString());
-
-                if (response.error) {
-                    throw response.error;
-                }
-
-                resolve({
-                    keys: keys,
-                    deviceId: deviceId,
-                    response: response
-                });
-
-                client.end();
-            });
-
-            client.on('end', function () {
-                log('Disconnected from ' + host + ':' + port);
-            });
-
-        });
     };
 
     return {
@@ -143,30 +90,14 @@ function Uploader(dataPath) {
             paths.sshPath = path;
         },
 
-        register: function (config) {
-            // TODO: the host and port need to be retrieved from the configuration
-            // NOTE: DONE. Pass those info in config and it will override the host and the port below
-            var opts = _.merge({}, {
-                host: 'ts.door43.org',
-                port: 9095
-            }, config);
-
-            var getDeviceId = opts.deviceId ? Promise.resolve(opts.deviceId) : this.getDeviceId();
+        generateKeys: function (opts) {
+            var getDeviceId = (opts && opts.deviceId) ? Promise.resolve(opts.deviceId) : utils.getDeviceId();
 
             return getDeviceId.then(function (deviceId) {
-
-                return readKeyPair().then(function (keys) {
-                    return {
-                        keys: keys,
-                        deviceId: deviceId
-                    };
-                }).catch(function (err) {
-                    var sendReg = sendRegistrationRequest.bind(null, opts.host, opts.port, deviceId);
-
-                    return createKeyPair(deviceId).then(sendReg);
-                }).then(function (reg) {
-                    reg.paths = paths;
-                    return reg;
+                return readKeyPair().catch(function (err) {
+                    return createKeyPair(deviceId);
+                }).then(function (keys) {
+                    return {keys, deviceId, paths};
                 });
             });
         },
@@ -175,18 +106,9 @@ function Uploader(dataPath) {
          * Deletes the ssh keys. They will be regenerated/registered next time we publish
          */
         destroyKeys: function () {
-            rimraf.sync(paths.sshPath);
-        },
-
-        verifyProfile: function (profile) {
-            return profile.getName() !== '' && profile.getEmail() !== '';
-        },
-
-        getDeviceId: function() {
-            return getMac().then(function (mac) {
-                return mac.replace(/-|:/g, '');
-            });
+            return utils.rm(paths.sshPath);
         }
+        
     };
 }
 

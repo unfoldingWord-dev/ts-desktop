ba4835233606130ada93dbb5a8dddae048e2ffab
Chris Whitfield <cdwhitfield73@gmail.com>
2016-04-07 14:19:07 -0700

    Updated exporter module
    Former-commit-id: f2bedb640ca6e4b94f7958ae40d33f00b0a6742b

--- src/js/exporter.js
+++ src/js/exporter.js
@@ -1,105 +1,27 @@
 'use strict';
 
 var _ = require('lodash'),
-    fs = require('fs'),
     path = require('path'),
-    mkdirP = require('mkdirp'),
-    rimraf = require('rimraf'),
     archiver = require('archiver'),
     utils = require('lib/utils'),
-    git = require('../js/git')(),
-    wrap = utils.promisify,
-    guard = utils.guard;
-
-var map = guard('map'),
-    indexBy = guard('indexBy'),
-    flatten = guard('flatten'),
-    compact = guard('compact');
+    git = require('../js/git')();
 
 function ExportManager(configurator) {
 
-    var write = wrap(fs, 'writeFile'),
-        read = wrap(fs, 'readFile'),
-        mkdirp = wrap(null, mkdirP),
-        rm = wrap(null, rimraf),
-        readdir = wrap(fs, 'readdir'),
-        stat = wrap(fs, 'stat'),
-        isDir = function (f) {
-            return stat(f).then(function (s) {
-                return s.isDirectory();
-            });
-        },
-        isVisibleDir = function (f) {
-            return isDir(f).then(function (isFolder) {
-                var name = path.parse(f).name,
-                    isHidden = /^\..*/.test(name);
-
-                return (isFolder && !isHidden) ? f : false;
-            });
-        },
-        filterDirs = function (dirs) {
-            return Promise.all(_.map(dirs, isVisibleDir)).then(compact());
-        },
-        readdirs = function (dirs) {
-            return Promise.all(_.map(dirs, function (d) {
-                return readdir(d).then(map(function (f) {
-                    return path.join(d, f);
-                }));
-            }));
-        },
-        toJSON = _.partialRight(JSON.stringify, null, '\t'),
-        fromJSON = JSON.parse.bind(JSON),
-
-        config = (function (prefix) {
-            var isUW = _.partial(_.startsWith, _, prefix, 0),
-                isChunk = function (filename) {
-                    return _.endsWith(filename, '.txt');
-                };
-
-            return {
-                filterProjects: _.partial(_.filter, _, isUW),
-
-                filterChapters: filterDirs,
-
-                filterChunks: _.partial(_.filter, _, isChunk),
-
-                get targetDir () {
-                    return configurator.getValue('targetTranslationsDir');
-                },
-
-
-                makeProjectPaths: function (meta) {
-                    var filename = meta.unique_id;
-                    return this.makeProjectPathsForProject(filename);
-                },
-
-                makeProjectPathsForProject: function (project) {
-                    var targetDir = this.targetDir,
-                        projectDir = path.join(targetDir, project);
-
-                    return {
-                        parentDir: targetDir,
-                        projectDir: projectDir,
-                        manifest: path.join(projectDir, 'manifest.json'),
-                        license: path.join(projectDir, 'LICENSE.md')
-                    };
-
-                }
-            };
-        })('uw-');
+    var targetDir = configurator.getValue('targetTranslationsDir'),
+        mkdirp = utils.fs.mkdirs,
+        rm = utils.fs.remove,
+        toJSON = _.partialRight(JSON.stringify, null, '\t');
 
     return {
 
-        getPaths: function(meta) {
-            return config.makeProjectPaths(meta);
-        },
-
         backupTranslation: function (meta, filePath) {
-            let paths = this.getPaths(meta),
-                name = meta.unique_id;
+            var paths = utils.makeProjectPaths(targetDir, meta);
+            var name = meta.unique_id;
 
             return new Promise(function(resolve, reject) {
-                let output = fs.createWriteStream(filePath),
+                // need to fix
+                var output = fs.createWriteStream(filePath),
                     archive = archiver.create('zip'),
                     manifest = {
                         generator: {
@@ -118,10 +40,6 @@ function ExportManager(configurator) {
             });
         },
 
-        /*
-         * Store projects in automatic backup folder if there's any change
-         * @param list: projectlist property
-         */
         backupProjects: function(list) {
             let _this = this,
                 backupDir = configurator.getUserPath('datalocation', 'automatic_backups');
@@ -145,7 +63,8 @@ function ExportManager(configurator) {
             };
 
             let promises = _.map(list, function(meta) {
-                let sourceDir = _this.getPaths(meta).projectDir,
+                let paths = utils.makeProjectPaths(targetDir, meta);
+                let sourceDir = paths.projectDir,
                     projectFolder = path.basename(sourceDir),
                     targetDir = path.join(backupDir, projectFolder),
                     doBackup = _this.backupTranslation.bind(_this, meta);
@@ -165,20 +84,10 @@ function ExportManager(configurator) {
             return Promise.all(promises);
         },
 
-        /**
-         *
-         * @param translation an array of frames
-         * @param meta the target translation manifest and other info
-         * @param filePath the path where the export will be saved
-         * @param mediaServer is the location of the media files
-         * @returns {Promise.<boolean>}
-         */
         exportTranslation: function (translation, meta, filePath, mediaServer) {
 
-            var isTranslation = this.isTranslation(meta);
-
             return new Promise(function(resolve, reject) {
-                if (isTranslation) {
+                if (meta.project_type_class === "standard") {
 
                     if (meta.format === 'markdown') {
 
@@ -278,10 +187,6 @@ function ExportManager(configurator) {
                     reject('We do not support exporting this project type yet');
                 }
             });
-        },
-
-        isTranslation: function (meta) {
-            return !meta.type.id || meta.type.id === 'text';
         }
     };
 }

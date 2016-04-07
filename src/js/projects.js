7977d9072a4d0c2c54e78fd8174aef29feca5c14
Chris Whitfield <cdwhitfield73@gmail.com>
2016-04-06 17:54:09 -0700

    Setup importer module
    Former-commit-id: f2ac529564de137d5249005aeab3cbec8a778ab6

--- src/js/projects.js
+++ src/js/projects.js
@@ -300,61 +300,6 @@ function ProjectsManager(dataManager, configurator, srcDir) {
             });
         },
 
-        /**
-         * Imports a tstudio archive
-         * @param filePath {String} the path to the archive
-         * @returns {Promise}
-         */
-        restoreTargetTranslation: function(filePath) {
-            let zip = new AdmZip(filePath),
-                tmpDir = configurator.getValue('tempDir'),
-                targetDir = configurator.getValue('targetTranslationsDir'),
-                basename = path.basename(filePath, '.tstudio'),
-                extractPath = path.join(tmpDir, basename);
-
-            return tstudioMigrator.listTargetTranslations(filePath)
-                .then(function(targetPaths) {
-                    // NOTE: this will eventually be async
-                    zip.extractAllTo(extractPath, true);
-                    return targetPaths;
-                })
-                .then(function (targetPaths) {
-                    return _.map(targetPaths, function (targetPath) {
-                        var parentDir = extractPath;
-                        var projectDir = path.join(extractPath, targetPath);
-                        var manifest = path.join(projectDir, 'manifest.json');
-                        var license = path.join(projectDir, 'LICENSE.md');
-                        return {parentDir, projectDir, manifest, license};
-                    });
-                })
-                .then(targetTranslationMigrator.migrateAll)
-                .then(function (results) {
-                    if (!results.length) {
-                        throw new Error ("Could not restore this project");
-                    }
-                    return results;
-                })
-                .then(function (results) {
-                    return _.map(results, function (result) {
-                        return result.paths.projectDir.substring(result.paths.projectDir.lastIndexOf(path.sep) + 1);
-                    });
-                })
-                .then(function (targetPaths) {
-                    return _.map(targetPaths, function(p) {
-                        let tmpPath = path.join(extractPath, p),
-                            targetPath = path.join(targetDir, p);
-
-                        return utils.move(tmpPath, targetPath, {clobber: true});
-                    });
-                })
-                .then(function (list) {
-                    return Promise.all(list);
-                })
-                .then(function () {
-                    return rm(tmpDir);
-                });
-        },
-
         fileExists: function (filePath) {
             return stat(filePath).then(function (){
                 return true;

4bf67c74602fc509485ae76bf4f4aee845c265e9
cdwhitfield73 <cdwhitfield73@gmail.com>
2016-04-14 12:14:37 -0700

    Major Bug Fix and more... (#549)
    * Make basic Import UI
    
    * Basic import function for backups
    
    * Incorporated USFM import code
    
    * Setting up USFM importing
    
    * Fix for race condition when saving
    
    * Changes to import and backup
    
    * Testing import of USFM
    
    * Fixes for USFM import
    
    * Minor fix
    
    * Updates to Importer
    
    * Changes in saving process
    
    * Fix for timing issue with new Projects
    
    * Added progress bar to source switching
    
    * Hide import for now

--- src/js/lib/utils.js
+++ src/js/lib/utils.js
@@ -360,6 +360,18 @@
                 license: path.join(projectDir, 'LICENSE.md')
             };
 
+        },
+
+        getTimeStamp: function () {
+            var date = new Date(),
+                year = date.getFullYear(),
+                month = ("0" + (date.getMonth() + 1)).slice(-2),
+                day = ("0" + date.getDate()).slice(-2),
+                hour = ("0" + date.getHours()).slice(-2),
+                minutes = ("0" + date.getMinutes()).slice(-2),
+                seconds = ("0" + date.getSeconds()).slice(-2);
+
+            return year + '-' + month + '-' + day + '_' + hour + '.' + minutes + '.' + seconds;
         }
     };
 

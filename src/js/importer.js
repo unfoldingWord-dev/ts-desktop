da6f50fbc95538b767bd3e8b49546fa4befa6a17
Chris Whitfield <cdwhitfield73@gmail.com>
2016-05-05 16:00:38 -0700

    Refactor restore function
    Former-commit-id: 9fda003b92ae8305882b68b81447e47646453138

--- src/js/importer.js
+++ src/js/importer.js
@@ -12,52 +12,6 @@ function ImportManager(configurator, migrator, dataManager) {
 
     return {
 
-        restoreFromBackup: function(filePath) {
-            var zip = new AdmZip(filePath),
-                tmpDir = configurator.getValue('tempDir'),
-                targetDir = configurator.getValue('targetTranslationsDir'),
-                basename = path.basename(filePath, '.tstudio'),
-                extractPath = path.join(tmpDir, basename);
-
-            return migrator.listTargetTranslations(filePath)
-                .then(function(targetPaths) {
-                    // NOTE: this will eventually be async
-                    zip.extractAllTo(extractPath, true);
-                    return targetPaths;
-                })
-                .then(function (targetPaths) {
-                    return _.map(targetPaths, function (targetPath) {
-                        return utils.makeProjectPaths(extractPath, targetPath);
-                    });
-                })
-                .then(migrator.migrateAll.bind(migrator))
-                .then(function (results) {
-                    if (!results.length) {
-                        throw new Error ("Could not restore this project");
-                    }
-                    return results;
-                })
-                .then(function (results) {
-                    return _.map(results, function (result) {
-                        return result.paths.projectDir.substring(result.paths.projectDir.lastIndexOf(path.sep) + 1);
-                    });
-                })
-                .then(function (targetPaths) {
-                    return _.map(targetPaths, function(p) {
-                        var tmpPath = path.join(extractPath, p),
-                            targetPath = path.join(targetDir, p);
-
-                        return utils.fs.move(tmpPath, targetPath, {clobber: true});
-                    });
-                })
-                .then(function (list) {
-                    return Promise.all(list);
-                })
-                .then(function () {
-                    return utils.fs.remove(tmpDir);
-                });
-        },
-
         extractBackup: function(filePath) {
             var zip = new AdmZip(filePath),
                 tmpDir = configurator.getValue('tempDir'),

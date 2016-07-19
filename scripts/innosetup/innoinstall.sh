b29c396d5c24837a38e11a02d66f0a4b9aa703b7
Joel Lonbeck <joel@neutrinographics.com>
2016-07-19 15:46:24 -0700

    tests

--- scripts/innosetup/innoinstall.sh
+++ scripts/innosetup/innoinstall.sh
@@ -4,7 +4,7 @@ rm -rf /tmp/inno
 mkdir /tmp/inno
 cd /tmp/inno
 
-wget -O is.exe http://www.jrsoftware.org/download.php/is.exe
+wget --no-check-certificate -O is.exe http://www.jrsoftware.org/download.php/is.exe
 innoextract is.exe
 mkdir -p ~/".wine/drive_c/inno"
 cp -a app/* ~/".wine/drive_c/inno"

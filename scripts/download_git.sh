60231871fb8b46d88c7c19dd1295469fe79d7b92
Joel Lonbeck <joel@neutrinographics.com>
2016-07-19 17:38:05 -0700

    tests

--- scripts/download_git.sh
+++ scripts/download_git.sh
@@ -4,5 +4,5 @@ dir=$1
 version=$2
 osbits=$3
 
-mkdir dir
+mkdir $dir
 wget wget -O "$dir/git-$osbits-bit.exe" "https://github.com/git-for-windows/git/releases/download/v$version.windows.1/Git-$version-$osbits-bit.exe"

b45501aa22375592791498731c6141081b3064ec
Chris Whitfield <cdwhitfield73@gmail.com>
2016-10-31 14:21:51 -0700

    Fix for issue 873, tq data

--- src/js/database.js
+++ src/js/database.js
@@ -297,10 +297,47 @@ function DataManager(db, resourceDir, apiURL, sourceDir) {
             }
         },
 
-        getSourceHelps: function (source, type) {
+        getSourceNotes: function (source) {
             var mythis = this;
-            var container = source.language_id + "_" + source.project_id + "_" + type;
+            var container = source.language_id + "_" + source.project_id + "_tn";
             var frames = this.extractContainer(container);
+
+            frames.forEach(function (item) {
+                if (item.content) {
+                    item.content = mythis.parseHelps(item.content);
+                }
+            });
+
+            return frames;
+        },
+
+        getSourceQuestions: function (source) {
+            var mythis = this;
+            var container = source.language_id + "_" + source.project_id + "_tq";
+            var markers = this.getChunkMarkers(source.project_id);
+            var frames = this.extractContainer(container);
+
+            frames.forEach(function (frame) {
+                var lastverse = "01";
+                var stop = false;
+
+                markers.forEach(function (marker) {
+                    if (stop || frame.chapter < marker.chapter || (frame.chapter === marker.chapter && frame.chunk < marker.verse)) {
+                        stop = true;
+                    } else {
+                        lastverse = marker.verse;
+                    }
+                });
+                frame.chunk = lastverse;
+            });
+
+            for (var i = 1; i < frames.length; i++) {
+                if (frames[i].chapter === frames[i-1].chapter && frames[i].chunk === frames[i-1].chunk) {
+                    frames[i-1].content = frames[i-1].content + "\n\n" + frames[i].content;
+                    frames.splice(i, 1);
+                    i--;
+                }
+            }
 
             frames.forEach(function (item) {
                 if (item.content) {

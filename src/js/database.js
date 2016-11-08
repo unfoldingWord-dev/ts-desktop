f1539dc4847568332b019f6b08cf455cc22ac85d
Chris Whitfield <cdwhitfield73@gmail.com>
2016-11-08 10:53:49 -0800

    Attempt at reorganzing source modal data

--- src/js/database.js
+++ src/js/database.js
@@ -112,6 +112,7 @@ function DataManager(db, resourceDir, apiURL, sourceDir) {
                         });
                 })
                 .then(function (containerpath) {
+                    source.updating = false;
                     if (containerpath) {
                         var manifest = path.join(containerpath, "package.json");
                         return utils.fs.readFile(manifest)

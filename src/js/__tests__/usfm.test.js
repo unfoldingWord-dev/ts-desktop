091f0c66881155e5a60912693b208333533a30ac
Joel Lonbeck <joel@neutrinographics.com>
2019-03-19 16:22:08 -0700

    added basic usfm generator

---
+++ src/js/__tests__/usfm.test.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var {generateProjectUSFM, fileComparator} = require('../usfm');
+
+describe('Project usfm generator', () => {
+    it('produces usfm', () => {
+        var usfm = generateProjectUSFM(path.join(__dirname, './usfmProject'));
+        expect(usfm).toEqual('\\h Philemon\n' +
+            '\\s1 Chapter 1\n' +
+            '\\c 1 \n' +
+            '\\v 1 Paul, a prisoner of Christ Jesus, and the brother Timothy to Philemon, our dear friend and fellow worker, \n' +
+            '\\v 2 and to Apphia our sister, and to Archippus our fellow soldier, and to the church that meets in your home. \n' +
+            '\\v 3 May grace be to you and peace from God our Father and the Lord Jesus Christ.\n' +
+            '\\s1 Chapter 10\n' +
+            '\\c 10 \n' +
+            '\\v 1 This is another verse \n' +
+            '\\v 2 And another.');
+    });
+});
+
+
+describe('file sorter', () => {
+    it('checks compare conditions', () => {
+        expect(fileComparator('front','back')).toEqual(-1);
+        expect(fileComparator('back','front')).toEqual(1);
+        expect(fileComparator('front','01')).toEqual(-1);
+        expect(fileComparator('back','01')).toEqual(1);
+        expect(fileComparator('01','front')).toEqual(1);
+        expect(fileComparator('01','back')).toEqual(-1);
+        expect(fileComparator('01','02')).toEqual(-1);
+        expect(fileComparator('02','01')).toEqual(1);
+        expect(fileComparator('10','01')).toEqual(1);
+        expect(fileComparator('01','10')).toEqual(-1);
+        expect(fileComparator('01','01')).toEqual(0);
+    });
+
+    it('sorts an array', () => {
+        expect(['01', 'front'].sort(fileComparator)).toEqual(['front', '01']);
+        expect(['01', 'back'].sort(fileComparator)).toEqual(['01', 'back']);
+        expect(['back', 'front'].sort(fileComparator)).toEqual(['front', 'back']);
+        expect(['front', '01'].sort(fileComparator)).toEqual(['front', '01']);
+        expect(['01', 'title'].sort(fileComparator)).toEqual(['title', '01']);
+    });
+});
---
+++ src/js/usfm.js
@@ -0,0 +1,93 @@
+var path = require('path');
+var fs = require('fs-extra');
+
+/**
+ * Produces USFM from a project
+ * @param projectPath
+ * @return {string}
+ */
+function generateProjectUSFM(projectPath) {
+    var srcDir = path.join(projectPath, '.apps/translationStudio');
+    var usfm = concatFiles(srcDir);
+
+    usfm = usfm.replace(/(\\v\s+)/g, '\n$1');
+
+    return usfm;
+}
+
+/**
+ * Compares two file names to determine sort order
+ * @param a
+ * @param b
+ * @returns {number}
+ */
+function fileComparator(a, b) {
+    // front and back data
+    var aIsFront = a === 'front';
+    var bIsFront = b === 'front';
+    var aIsBack = a === 'back';
+    var bIsBack = b === 'back';
+
+    // chapter sections
+    var aIsTitle = a.startsWith('title');// === 'title.txt';
+    var bIsTitle = b.startsWith('title');// === 'title.txt';
+
+    // compare book data
+    if (aIsFront) return -1;
+    if (bIsFront) return 1;
+    if (aIsBack) return 1;
+    if (bIsBack) return -1;
+
+    // compare chapter sections
+    if(aIsTitle) return -1;
+    if(bIsTitle) return 1;
+
+    // compare numbers
+    try {
+        var diff = parseInt(b) - parseInt(a);
+        if(diff > 0) {
+            return -1;
+        } else if (diff < 0) {
+            return 1;
+        }
+    } catch (e) {
+        console.error('Unexpected value. Cannot compare "' + a + '" with "' + b + '"' , e);
+    }
+    return 0 ;
+}
+
+/**
+ * walks over every folder/file in the directory and
+ * concatenates all the files
+ * @param filepath
+ * @returns {string}
+ */
+function concatFiles(filepath) {
+    var usfm = '';
+    var files = fs.readdirSync(filepath);
+    files.sort(fileComparator);
+    for (var i = 0, len = files.length; i < len; i++) {
+        var itemPath = path.join(filepath, files[i]);
+        // TODO: concat files
+        if(fs.statSync(itemPath).isDirectory()) {
+            usfm += '\n' + concatFiles(itemPath).replace(/(^\s|\s$)/, '');
+        } else {
+            var fileData = fs.readFileSync(itemPath).toString().replace(/(^\s|\s$)/, '');
+            if(files[i] === 'title.txt') {
+                if(path.basename(filepath) === 'front') {
+                    // book title
+                    usfm  += '\n\\h ' + fileData;
+                } else {
+                    // chapter headers
+                    usfm += '\n\\s1 ' + fileData;
+                }
+            } else {
+                usfm += '\n' + fileData;
+            }
+        }
+    }
+    return usfm.replace(/(^\s|\s$)/, '');
+}
+
+module.exports.generateProjectUSFM = generateProjectUSFM;
+module.exports.fileComparator = fileComparator;

0fbb68bd8ab6ae7cc9ccb0270cb818af94288cec
Joel <joel@neutrinographics.com>
2019-10-08 11:54:52 +0700

    removed old babel imports

--- src/js/require-es6.js
+++ src/js/require-es6.js
@@ -1,6 +1,6 @@
-require("@babel/register");
-require("core-js/stable");
-require("regenerator-runtime/runtime");
+// require("@babel/register");
+// require("core-js/stable");
+// require("regenerator-runtime/runtime");
 
 module.exports = function(module) {
     return require(module);
